39813fa290613dd5cd947a16e2c850287622e264
Phoenix67rus <tm87rus67@gmail.com>
2024-02-15 21:42:04 +0300

    Создал API_Token. Сделал функцию, отвечающую за приветственное сообщение.

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-#
+import telebot
+from config import TOKEN
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, 'Здравствуй, добрый человек! Добро пожаловать в бота приюта "Верность"')
+
+if __name__ == '__main__':
+    print('Бот запущен!')
+    bot.infinity_polling()
