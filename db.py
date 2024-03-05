b0476a803f530965b0134d1f6b9bf863cee4603f
phoenixtheone <tm87rus67@gmail.com>
2024-03-05 22:31:47 +0300

    Подключили базу данных, проверили работоспособность

---
+++ db.py
@@ -0,0 +1,7 @@
+import pymysql
+
+connection = pymysql.connect(host='147.45.105.54',
+                             user='gen_user',
+                             password='mfohmdu%E3$b\k',
+                             database='default_db',
+                             port=3306)
